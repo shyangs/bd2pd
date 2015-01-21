0321fd638c944fc7fe56757119f69aeb4453542b
shyangs <shyangs@gmail.com>
2015-01-22 07:40:05 +0800

    支援 IE 和 Chrome (相容性修復)、網頁加上問題反饋連結
    Default function parameters 目前只有Firefox25+支援.

--- js/lib.js
+++ js/lib.js
@@ -1,5 +1,9 @@
 var 注音符號表 = "ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙㄧㄨㄩㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦˉˊˇˋ˙";
 
+var initFuncParam = function(param, defaultValue){
+  return ((typeof(param) === 'undefined') ? defaultValue : param);
+};
+
 var 字串中是否含有陣列中任一元素 = function(字串, 陣列){
   var ii = 陣列.length;
   while(ii--){
